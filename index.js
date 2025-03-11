8aca0f8e18d8d5e5ec97b21898ae2f1e960b09cc
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-03-11 15:03:44 +0300

    added some string to uppercase and another to lowercase

---
+++ index.js
@@ -0,0 +1,60 @@
+function shout(string) {
+   
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); 
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  
+  
+  module.exports = { logShout };
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    }
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    }
+  
+    
+    if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    }
+  
+    
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    
+    if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    }
+  
+    
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    }
+  
+
+    if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    }
+  
+  }
